faf2b6cdbc379cdd224c3345f4de7712febb5f7f
Carava1998 <cesar_ramos_v@hotmail.com>
2021-06-07 01:00:39 -0500

    STOP plz

--- carsim_gazebo/src/imageProcessing.py
+++ carsim_gazebo/src/imageProcessing.py
@@ -3,6 +3,7 @@ from numpy.core.numeric import False_
 import rospy
 import cv2
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import sys
@@ -13,31 +14,44 @@ import os
 
 bridge = CvBridge()
 
+pub = rospy.Publisher('carsim/cmd_vel', Twist, queue_size = 1)
+
+def publishStopPlz():
+    twist = Twist()
+    print("Ajua")
+    twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+    twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+    pub.publish(twist)
+
+
+def getStopDistance(approx):
+    p1 = approx[0][0]
+    p2 = approx[4][0]
+    x1 = p1[0]
+    y1 = p1[1]
+    x2 = p2[0]
+    y2 = p2[1]
+
+    return ((((x2 - x1 )**2) + ((y2-y1)**2) )**0.5)
 
 
 def findStopSign(cv_image):
     gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
     dilate = cv2.dilate(gray,np.ones((3, 3), np.uint8))
     erode = cv2.erode(dilate,np.ones((7, 7), np.uint8))
-    found = False
-    """ cv2.imshow('img1',gray) """
     ret,thresh1 = cv2.threshold(erode, 0, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    """ cv2.imshow('thresh1', thresh1) """
     contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt,0.04*cv2.arcLength(cnt,True),True)
         if len(approx) == 8:
-            found = True
-            cv2.drawContours(cv_image, [cnt], 0, (0, 255, 0), 6)
-            cv2.imshow('img1',erode)
-            cv2.imshow('thresh1', thresh1)
-            cv2.imshow('sign', cv_image)
-            cv2.waitKey(0)
-    if found:
-        print("found")
-    else:
-        print("Not found")
-    """ cv2.imshow('sign', cv_image)  """
+            dist = getStopDistance(approx)
+            if(dist >= 41):
+                publishStopPlz()
+                cv2.drawContours(cv_image, [cnt], 0, (0, 255, 0), 6)
+                cv2.imshow('img1',erode)
+                cv2.imshow('thresh1', thresh1)
+                cv2.imshow('sign', cv_image)
+                cv2.waitKey(0)
 
 
 
